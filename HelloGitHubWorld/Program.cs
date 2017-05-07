73229ea52fe448ee96e4d8cb8a3ee796b030e82b
Adrian Jackson <a13jacks@yahoo.co.uk>
2017-05-07 19:39:01 +0100

    This is the initial commit of the HelloGitHubWorld soluition

---
+++ HelloGitHubWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitHubWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHUb from Visual Studio 2013!");
+        }
+    }
+}
